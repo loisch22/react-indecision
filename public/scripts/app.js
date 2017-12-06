3d3adc0516b171ea64277c16d94e1d2a537a36d6
Lois Choi <loisch22@gmail.com>
2017-12-06 14:21:26 -0800

    my version of building toggle visibility

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,46 @@
 'use strict';
 
 var app = {
-    title: 'Visibility Toggle'
+    title: 'Visibility Toggle',
+    text: 'Hey this works!',
+    show: 'Show details',
+    hide: 'Hide details'
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    )
-);
+var visibility = false;
+
+var onShowDetails = function onShowDetails() {
+    if (visibility === false) {
+        visibility = true;
+    } else {
+        visibility = false;
+    }
+    render();
+};
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        React.createElement(
+            'button',
+            { onClick: onShowDetails },
+            visibility ? 'Hide details' : 'Show details'
+        ),
+        React.createElement(
+            'p',
+            null,
+            visibility ? 'Hey it works!' : ''
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+render();
--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,12 +1,35 @@
 const app = {
-    title: 'Visibility Toggle'
+    title: 'Visibility Toggle',
+    text: 'Hey this works!',
+    show: 'Show details',
+    hide: 'Hide details'
 };
 
-const template = (
-  <div>
-      <h1>{app.title}</h1>
-  </div>
-);
+let visibility = false;
+
+const onShowDetails = () => {
+    if(visibility === false) {
+        visibility = true;
+    } else {
+        visibility = false;
+    }
+    render();
+};
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+const render = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            <button onClick={onShowDetails}>
+                {visibility ? 'Hide details' : 'Show details'}
+            </button>
+            <p>{visibility ? 'Hey it works!' : ''}</p>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+render();
+

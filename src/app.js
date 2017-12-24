66e5af48d0c70b7e020a31fe6b9370c397cedd9a
Lois Choi <loisch22@gmail.com>
2017-12-24 12:42:51 -0800

    create props, nest props to list out options

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 class IndecisionApp extends React.Component {
     render() {
         const title = 'Indecision';
-        const subtitle = 'Put your life in the hands of a computer'
+        const subtitle = 'Put your life in the hands of a computer';
         const options = ['Thing one', 'Thing two', 'Thing four'];
 
         return (
@@ -41,7 +41,7 @@ class Options extends React.Component {
         return (
             <div>
                 {
-                    this.props.options.map((option) => <p key={option}>{option}</p>)
+                    this.props.options.map((option) => <Option key={option} optionText={option}/>)
                 }
             </div>
         );
@@ -52,7 +52,7 @@ class Option extends React.Component {
     render() {
         return (
           <div>
-              Option component
+              {this.props.optionText}
           </div>
         );
     }

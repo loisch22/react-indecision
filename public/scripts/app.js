3f1bb3d5caaa0fa4641cf0a5dae1530a4f7f9792
Lois Choi <loisch22@gmail.com>
2017-12-27 16:21:24 -0800

    change visibility to use component state

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -63,32 +63,3 @@ var VisibilityToggle = function (_React$Component) {
 }(React.Component);
 
 ReactDOM.render(React.createElement(VisibilityToggle, null), document.getElementById('app'));
-
-// const app = {
-//     title: 'Visibility Toggle',
-//     text: 'Hey this works!'
-// };
-//
-// let visibility = false;
-//
-// const onShowDetails = () => {
-//     visibility = !visibility;
-//     render();
-// };
-//
-// const appRoot = document.getElementById('app');
-//
-// const render = () => {
-//     const template = (
-//         <div>
-//             <h1>{app.title}</h1>
-//             <button onClick={onShowDetails}>
-//                 {visibility ? 'Hide details' : 'Show details'}
-//             </button>
-//             {(visibility && true) && <p>{app.text}</p> }
-//         </div>
-//     );
-//     ReactDOM.render(template, appRoot);
-// };
-//
-// render();
--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -31,32 +31,3 @@ class VisibilityToggle extends React.Component {
 }
 
 ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
-
-// const app = {
-//     title: 'Visibility Toggle',
-//     text: 'Hey this works!'
-// };
-//
-// let visibility = false;
-//
-// const onShowDetails = () => {
-//     visibility = !visibility;
-//     render();
-// };
-//
-// const appRoot = document.getElementById('app');
-//
-// const render = () => {
-//     const template = (
-//         <div>
-//             <h1>{app.title}</h1>
-//             <button onClick={onShowDetails}>
-//                 {visibility ? 'Hide details' : 'Show details'}
-//             </button>
-//             {(visibility && true) && <p>{app.text}</p> }
-//         </div>
-//     );
-//     ReactDOM.render(template, appRoot);
-// };
-//
-// render();

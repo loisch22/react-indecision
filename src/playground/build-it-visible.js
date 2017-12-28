8884b99ce90e6ecc777da70e4fb0881b88a5a014
Lois Choi <loisch22@gmail.com>
2017-12-27 16:12:43 -0800

    practice creating own component for visibility and add state

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,29 +1,62 @@
-const app = {
-    title: 'Visibility Toggle',
-    text: 'Hey this works!'
-};
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.title = 'Visibility Toggle';
+    this.text = 'Here it is!'
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    };
+  }
 
-let visibility = false;
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      };
+    });
+  }
 
-const onShowDetails = () => {
-    visibility = !visibility;
-    render();
-};
-
-const appRoot = document.getElementById('app');
-
-const render = () => {
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            <button onClick={onShowDetails}>
-                {visibility ? 'Hide details' : 'Show details'}
-            </button>
-            {(visibility && true) && <p>{app.text}</p> }
-        </div>
+  render() {
+    return (
+      <div>
+        <h1>{this.title}</h1>
+        <button onClick={this.handleToggleVisibility}>
+        {this.state.visibility ? 'Hide details' : 'Show details'}
+        </button>
+        {(this.state.visibility && true) && <p>{this.text}</p>}
+      </div>
     );
-    ReactDOM.render(template, appRoot);
-};
+  }
+}
 
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
+// const app = {
+//     title: 'Visibility Toggle',
+//     text: 'Hey this works!'
+// };
+//
+// let visibility = false;
+//
+// const onShowDetails = () => {
+//     visibility = !visibility;
+//     render();
+// };
+//
+// const appRoot = document.getElementById('app');
+//
+// const render = () => {
+//     const template = (
+//         <div>
+//             <h1>{app.title}</h1>
+//             <button onClick={onShowDetails}>
+//                 {visibility ? 'Hide details' : 'Show details'}
+//             </button>
+//             {(visibility && true) && <p>{app.text}</p> }
+//         </div>
+//     );
+//     ReactDOM.render(template, appRoot);
+// };
+//
+// render();
--- src/playground/counter-ex.js
+++ src/playground/counter-ex.js
@@ -30,11 +30,6 @@ class Counter extends React.Component {
         count: 0
       };
     });
-    this.setState((prevState) => {
-      return {
-        count: prevState.count + 1
-      };
-    });
   }
 
   render() {
@@ -51,36 +46,3 @@ class Counter extends React.Component {
 
 
 ReactDOM.render(<Counter/>, document.getElementById('app'));
-
-
-// let count = 0;
-// const addOne = () => {
-//     count++;
-//     renderCounterApp();
-// };
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-//     console.log('minusOne', count);
-// };
-// const reset = () => {
-//     count = 0;
-//     renderCounterApp();
-//     console.log('reset', count);
-// };
-//
-// const appRoot = document.getElementById('app');
-//
-// const renderCounterApp = () => {
-//     const templateTwo = (
-//         <div>
-//             <h1>Count: {count}</h1>
-//             <button onClick={addOne}>+1</button>
-//             <button onClick={minusOne}>-1</button>
-//             <button onClick={reset}>Reset</button>
-//         </div>
-//     );
-//     ReactDOM.render(templateTwo, appRoot);
-// };
-//
-// renderCounterApp();

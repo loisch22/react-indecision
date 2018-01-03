c0b6673546adea510c5c0c1ec684924fb19e1eb9
Lois Choi <loisch22@gmail.com>
2018-01-03 12:46:19 -0800

    import validator module, useuse isEmail validator from validator library

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
-//default goes before named exports
-//default special - doesn't have to use 'subtract' since its the default, you can name it whatever you want
-// import subtract, { add, square } from './utils.js';
-//
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 10));
+//import npm modules
+//install library - validator
+//install React
+//steps: install - import - use
+//usually have to look at documentation to get what you want default export? named?
+//google library aka npm validator
 
-import isSenior, { isAdult, canDrink } from './person.js';
+//searches node_modules for same name and uses that library - default export
+//provide module name - see documentation for what this module covers
+import validator from 'validator';
 
-console.log('Adult? ', isAdult(18));
-console.log('Can drink? ', canDrink(30));
-console.log('Is senior? ', isSenior(65));
+console.log(validator.isEmail('test@gmail.com'));

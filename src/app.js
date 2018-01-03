4ac13dfc7c05473b666925ac47c580267ec753d1
Lois Choi <loisch22@gmail.com>
2018-01-03 15:16:28 -0800

    one way of passing ijsx through props

--- src/app.js
+++ src/app.js
@@ -2,4 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+//pass jsx from other component
+const Layout = (props) => {
+  return (
+    <div>
+      <p>header</p>
+      {props.content}
+      <p>footer</p>
+    </div>
+  );
+};
+
+const template = (
+  <div>
+    <h1>Page Title</h1>
+    <p>This is my page.</p>
+  </div>
+);
+
+ReactDOM.render(<Layout content={template}/>, document.getElementById('app'));

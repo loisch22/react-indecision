00ac23c83e7f89d1935ecddfa9185ae77c71ff09
Lois Choi <loisch22@gmail.com>
2018-01-03 13:06:17 -0800

    add module for babel-loader nr need to set up seperate config.js file for babel to add presets

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,18 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      //regular expression - target files that end in .js
+      //what files do we want to run this loader on? that's where test comes in
+      //. has special sign so \. before and $ tells at the end
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
   }
 };
+
+//loader - customize webpack behavior
+//ex. when webpack sees js file, run it through babel, convert es6 to es5 and jsx to reg js

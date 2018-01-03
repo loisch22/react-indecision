41c453164b4569d88a9eaebd05b96cb04db20b8b
Lois Choi <loisch22@gmail.com>
2018-01-03 14:46:31 -0800

    example of oldSyntax and newSyntax

--- src/app.js
+++ src/app.js
@@ -2,4 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class OldSyntax {
+  //new instance of this class needs this.value
+  constructor() {
+    this.name = 'Mike';
+  }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+// -----------
+
+class NewSyntax {
+  //key = value
+  name = 'Jen';
+}
+
+const newSyntax = new NewSyntax();
+console.log(newSyntax);

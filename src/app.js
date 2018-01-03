9800f714d4c27f8700477aa78424213e4119a886
Lois Choi <loisch22@gmail.com>
2018-01-03 14:55:11 -0800

    difference between creating function within class and not binding it to this using class property syntax

--- src/app.js
+++ src/app.js
@@ -8,18 +8,28 @@ class OldSyntax {
   //new instance of this class needs this.value
   constructor() {
     this.name = 'Mike';
+    this.getGreeting = this.getGreeting.bind(this);
+  }
+  getGreeting() {
+    return `Hi my name is ${this.name}`;
   }
 }
 
 const oldSyntax = new OldSyntax();
-console.log(oldSyntax);
+const getGreeting = oldSyntax.getGreeting;
+console.log(getGreeting());
 
 // -----------
 
 class NewSyntax {
   //key = value
   name = 'Jen';
+  //always bound to class instance w/o this
+  getGreeting = () => {
+    return `Hi my name is ${this.name}`;
+  }
 }
 
 const newSyntax = new NewSyntax();
-console.log(newSyntax);
+const newGetGreeting = newSyntax.getGreeting;
+console.log(newGetGreeting());

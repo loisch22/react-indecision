1c04235b4903aed533cc6a13a27f9c2f464b2a6c
Lois Choi <loisch22@gmail.com>
2018-01-03 12:32:45 -0800

    practice inline export default with person

--- public/bundle.js
+++ public/bundle.js
@@ -69,41 +69,40 @@
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils_js__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
 //default goes before named exports
 //default special - doesn't have to use 'subtract' since its the default, you can name it whatever you want
-
-
-console.log('app.js is running');
-console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["c" /* square */](4));
-console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["a" /* add */](100, 23));
-console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["b" /* default */](100, 10));
-
-// import { isAdult, canDrink } from './person.js';
+// import subtract, { add, square } from './utils.js';
 //
-// console.log('Adult? ', isAdult(18));
-// console.log('Can drink? ', canDrink(30));
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
+// console.log(subtract(100, 10));
+
+
+
+console.log('Adult? ', __WEBPACK_IMPORTED_MODULE_0__person_js__["c" /* isAdult */](18));
+console.log('Can drink? ', __WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](30));
+console.log('Is senior? ', __WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* default */](65));
 
 
 /***/ }),
-/* 1 */
+/* 1 */,
+/* 2 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-//exports - default exports
-console.log('utils.js is running');
+const isAdult = (age) => age >= 18;
+/* harmony export (immutable) */ __webpack_exports__["c"] = isAdult;
 
-const square = (x) => x * x;
-/* harmony export (immutable) */ __webpack_exports__["c"] = square;
 
-const add = (a, b) => a + b;
-/* harmony export (immutable) */ __webpack_exports__["a"] = add;
+const canDrink = (age) => age >= 21;
+/* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
 
-// const subtract = (a, b) => a - b;
 
-/* harmony default export */ __webpack_exports__["b"] = ((a, b) => a - b);
-// export { square, add, subtract as default };
-//can only have 0 or 1 default export
+/* harmony default export */ __webpack_exports__["b"] = ((age) => age >= 65);
+
+// export { isAdult, canDrink };
 
 
 /***/ })
--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 //default goes before named exports
 //default special - doesn't have to use 'subtract' since its the default, you can name it whatever you want
-import subtract, { add, square } from './utils.js';
-
-console.log('app.js is running');
-console.log(square(4));
-console.log(add(100, 23));
-console.log(subtract(100, 10));
-
-// import { isAdult, canDrink } from './person.js';
+// import subtract, { add, square } from './utils.js';
 //
-// console.log('Adult? ', isAdult(18));
-// console.log('Can drink? ', canDrink(30));
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
+// console.log(subtract(100, 10));
+
+import isSenior, { isAdult, canDrink } from './person.js';
+
+console.log('Adult? ', isAdult(18));
+console.log('Can drink? ', canDrink(30));
+console.log('Is senior? ', isSenior(65));

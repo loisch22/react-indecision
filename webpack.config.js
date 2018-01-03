17fcd5327bbb0090ed0d00513f477f0f699ccb46
Lois Choi <loisch22@gmail.com>
2018-01-03 11:43:36 -0800

    complete minimal webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,9 @@
-//entry point - where does app kick off aka src/app.js
-//tell output
-
-//load module by name
 const path = require('path');
 
-//contains path to current location aka indecisionapp proj folder
-console.log(path.join(__dirname, 'public'));
-
-//below is a 'node thing'
 module.exports = {
   entry: './src/app.js',
   output: {
-    //needs to be absolute path
-    path: '',
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   }
 };

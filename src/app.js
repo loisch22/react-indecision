801b7d0d166c8579e6100eb12ae916ed5722114d
Lois Choi <loisch22@gmail.com>
2018-01-04 13:02:56 -0800

    normalize.css sets all browsers to same base

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+//goes before our own style - sets all browser to same base
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 

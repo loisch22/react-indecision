aa87c11ad4ef839ac8df090d8c6a76cfe0c0de3b
Lois Choi <loisch22@gmail.com>
2018-01-04 15:10:22 -0800

    style modal and add word-break to keep text inside content

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,13 +5,14 @@ const OptionModal = (props) => (
   <Modal
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleCloseModal}
-    //amt of time to wait before closing
     closeTimeoutMS={200}
     contentLabel="Selected Option"
+    //add className overrides default styles
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleCloseModal}>Okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleCloseModal}>Okay</button>
   </Modal>
 );
 

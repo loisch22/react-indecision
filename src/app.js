e67ed9559c618489bd14537961fb122bcc336fef
Lois Choi <loisch22@gmail.com>
2018-01-04 13:15:48 -0800

    create variables for settings - generic settings applied across views

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
-//goes before our own style - sets all browser to same base
+//goes before our own style - sets all browser to same base - cross browser friendly
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
